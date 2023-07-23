1d73751267601007c23fff1be2cc5c886aed49c4
WuuShan <805490972@qq.com>
2023-07-24 01:54:46 +0800

    10

--- Lesson1_FirstApplication/Program.cs
+++ Lesson1_FirstApplication/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("你好，世界！");
+// 函数代码块 我们目前学习基础知识时
+// 所有的代码 都会写在 该 代码块中
+
+// 在控制台 打印出 你好世界这个内容
+// 在控制台 打印一行信息 打印信息结束后 自动空一行
+Console.WriteLine("你好世界！");
+
+Console.WriteLine("唐老狮");
+// 如果是双引号之间的内容 对于符号没有特别要求
+// 在控制台打印信息 但是打印完成后 不会自动空行
+Console.Write("你好，我喜欢你");
+Console.Write("我要好好学习，天天向上");
+
+Console.WriteLine("请玩家输入");
+
+// 检测玩家输入的代码
+Console.ReadLine();
+
+Console.WriteLine("玩家输入完毕");
